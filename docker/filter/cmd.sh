c40d4bedd5532c3b2f7e0246c2f166c412755fab
Miroljub <miroljub1995@gmail.com>
2020-05-25 13:22:20 +0200

    fix port typo

--- docker/filter/cmd.sh
+++ docker/filter/cmd.sh
@@ -13,6 +13,6 @@ stop)
     docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE_NAME | awk '{print $1 }' | xargs -I {} docker stop {}
     ;;
 start)
-    docker run -p 8003:8003 --name=filter --network="my-net" $IMAGE_NAME
+    docker run -p 8002:8002 --name=filter --network="my-net" $IMAGE_NAME
     ;;
 esac
