d57cacc8a31b84a6452de8c7c5fc51e7f1f3e80c
Miroljub <miroljub1995@gmail.com>
2020-06-03 11:20:37 +0200

    add api for mongo repl status

--- docker/hybrid-server/src/index.js
+++ docker/hybrid-server/src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+import { MongoClient } from 'mongodb'
 
 // Constants
 const PORT = 8080;
@@ -10,6 +11,19 @@ const app = express()
 app.get('/', (req, res) => {
     res.send(`Hello World, your ip: ${req.ip.toString()}\r\n`);
 });
+
+app.get('/mongo-repl-status', (req, res) => {
+    MongoClient.connect('mongodb://mongo-db-0:mongo.db:27017,mongo-db-1:mongo.db:27017,mongo-db-2:mongo.db:27017/?replicaSet=repl-set0', (err, db) => {
+        const adminDb = db.admin()
+        adminDb.command({ "replSetGetStatus": 1 }, (err, result) => {
+            res.setHeader('Content-Type', 'application/json')
+            if (err) {
+                res.send(JSON.stringify(err))
+            }
+            res.send(JSON.stringify(result))
+        })
+    })
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
