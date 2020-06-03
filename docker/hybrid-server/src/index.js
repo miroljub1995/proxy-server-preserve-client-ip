0a4cc8ec3a289730d05775cc4d7c41fb153b51bc
Miroljub <miroljub1995@gmail.com>
2020-06-03 11:36:12 +0200

    replace import with require

--- docker/hybrid-server/src/index.js
+++ docker/hybrid-server/src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const express = require('express');
-import { MongoClient } from 'mongodb'
+const express = require('express')
+const MongoClient = require('mongodb')
 
 // Constants
 const PORT = 8080;
