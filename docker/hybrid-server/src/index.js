85500e9ab1c9d5a8eed751ce71d562021169aeee
Miroljub <miroljub1995@gmail.com>
2020-06-03 15:36:04 +0200

    fix mongo uri

--- docker/hybrid-server/src/index.js
+++ docker/hybrid-server/src/index.js
@@ -6,8 +6,7 @@ const MongoClient = require('mongodb')
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-// const MONGO_URI = 'mongodb://mongo-db-0.mongo-db:27017,mongo-db-1.mongo-db:27017,mongo-db-2.mongo-db:27017/?replicaSet=repl-set0'
-const MONGO_URI = 'mongodb://mongo-db/?replicaSet=repl-set0'
+const MONGO_URI = 'mongodb://mongo-db-0.mongo-db:27017,mongo-db-1.mongo-db:27017,mongo-db-2.mongo-db:27017/?replicaSet=repl-set0'
 
 const app = express()
 app.get('/', (req, res) => {
