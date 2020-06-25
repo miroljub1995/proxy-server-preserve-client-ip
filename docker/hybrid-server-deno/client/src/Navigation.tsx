597072a1753558a1a75034864b6fb9c3b805e0d8
Miroljub <miroljub1995@gmail.com>
2020-06-25 23:00:26 +0200

    add navigation

---
+++ docker/hybrid-server-deno/client/src/Navigation.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+
+function Navigation() {
+  return (
+   <Navbar bg="light" expand="lg">
+     <Navbar.Brand href="/">Location saver</Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/saved">Saved</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    )
+}
+
+export default Navigation
