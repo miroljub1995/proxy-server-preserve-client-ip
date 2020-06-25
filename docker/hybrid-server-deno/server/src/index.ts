156deab18a13a92a5165f451cc01133672f5869e
Miroljub <miroljub1995@gmail.com>
2020-06-25 18:09:16 +0200

    update Dockerfile

--- docker/hybrid-server-deno/server/src/index.ts
+++ docker/hybrid-server-deno/server/src/index.ts
@@ -1,6 +1,9 @@
-import { serve } from "https://deno.land/std@0.58.0/http/server.ts"
-const s = serve({ port: 8080 })
-console.log("http://localhost:8080/")
-for await (const req of s) {
-    req.respond({ body: "Hello World\n" })
-}
+import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts"
+
+const app = new Application()
+app
+    .static("/", "../client/")
+    .get("/hello", (c) => {
+        return "Hello World!"
+    })
+    .start({ port: 8080 })
