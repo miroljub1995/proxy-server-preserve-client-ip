05563b22008fa903041622b285214a29d51105ac
Miroljub <miroljub1995@gmail.com>
2020-06-25 23:30:23 +0200

    added router

--- docker/hybrid-server-deno/client/src/App.tsx
+++ docker/hybrid-server-deno/client/src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import './App.css';
-import Navigation from './Navigation';
+import './App.css'
+import Navigation from './Navigation'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navigation />
-      <p>Hello world</p>
-    </div>
-  );
+      <Switch>
+        <Route path="/saved">
+          <p>Saved</p>
+        </Route>
+        <Route path="/">
+          <p>Home</p>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- docker/hybrid-server-deno/client/src/Navigation.tsx
+++ docker/hybrid-server-deno/client/src/Navigation.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import {
+  Navbar,
+  Nav
+} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Navigation() {
   return (
-   <Navbar bg="light" expand="lg">
-     <Navbar.Brand href="/">Location saver</Navbar.Brand>
-     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-     <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/saved">Saved</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    )
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Location saver</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/saved">Saved</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Navigation
