26c02ed296dfea8326da819b66dcf7175b884ec3
Miroljub <miroljub1995@gmail.com>
2020-07-06 23:21:13 +0200

    delete nodejs app

--- docker/hybrid-server/entry.sh
+++
@@ -1,9 +0,0 @@
-#setup for mmproxy
-ip rule add from 127.0.0.1/8 iif lo table 123
-ip route add local 0.0.0.0/0 dev lo table 123
-
-ip -6 rule add from ::1/128 iif lo table 123
-ip -6 route add local ::/0 dev lo table 123
-
-./go-mmproxy -l 0.0.0.0:8082 -4 127.0.0.1:8080 -v 2 &
-npm start
