34facff2e21f57048e1c9a6c90ba19ac79b76486
Miroljub <miroljub1995@gmail.com>
2020-07-06 22:52:16 +0200

    add build argument for react app use supervisor to run mmproxy

--- docker/hybrid-server-deno/cmd.sh
+++ docker/hybrid-server-deno/cmd.sh
@@ -9,7 +9,7 @@ case $1 in
 rebuild)
     docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE_NAME | awk '{print $1 }' | xargs -I {} docker rm {} &&
     docker rmi $IMAGE_NAME
-    docker build -t $IMAGE_NAME .
+    docker build -t $IMAGE_NAME --build-arg REACT_APP_API_ENDPOINT=$2 .
     ;;
 stop)
     docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE_NAME | awk '{print $1 }' | xargs -I {} docker stop {}
