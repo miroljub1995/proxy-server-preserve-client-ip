e46a4c4ca554538826806f479b8aba853daf6aa6
Miroljub <miroljub1995@gmail.com>
2020-07-06 23:24:53 +0200

    rename server app

--- docker/hybrid-server/cmd.sh
+++ docker/hybrid-server/cmd.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # consts
-IMAGE_NAME='local/pspcip/hybrid-server-deno'
-IMAGE_NAME_C_DEV='local/pspcip/hybrid-server-deno-c-dev'
-IMAGE_NAME_S_DEV='local/pspcip/hybrid-server-deno-s-dev'
+IMAGE_NAME='local/pspcip/hybrid-server'
 
 case $1 in
 rebuild)
@@ -15,6 +13,6 @@ stop)
     docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE_NAME | awk '{print $1 }' | xargs -I {} docker stop {}
     ;;
 start)
-    docker run -p 8082:8082 -p 8080:8080 --name=hybrid-server-deno --cap-add NET_ADMIN --network="my-net" -it $IMAGE_NAME
+    docker run -p 8082:8082 -p 8080:8080 --name=hybrid-server --cap-add NET_ADMIN --network="my-net" -it $IMAGE_NAME
     ;;
 esac
