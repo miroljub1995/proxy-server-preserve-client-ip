c9de9fff232dc848bd11779874e057c36af6d133
Miroljub <miroljub1995@gmail.com>
2020-07-07 21:23:40 +0200

    fix wrong db env

--- docker/hybrid-server/server/src/database.ts
+++ docker/hybrid-server/server/src/database.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
-const dbURI = Deno.env.get('DB_URL') || "mongodb://localhost:27017"
+const dbURI = Deno.env.get('DB_URI') || "mongodb://localhost:27017"
 
 export default function dbClient() {
   const client = new MongoClient()
