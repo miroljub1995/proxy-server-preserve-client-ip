048b755490533d60046c7a3b00f99557299e7a2c
Miroljub <miroljub1995@gmail.com>
2020-09-02 14:36:28 +0200

    added link to home post

--- docker/hybrid-server/client/src/components/PostList.tsx
+++ docker/hybrid-server/client/src/components/PostList.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { Row, Col, ListGroup } from 'react-bootstrap'
 import { Post } from '../api/types'
 import "./PostList.scss"
+import { Link } from 'react-router-dom'
 
 export default ({ posts, title }: { posts: Post[], title: string }) => (
   <Row>
     <Col className="justify-content-center">
       <h5 className="mb-4 text-center">{title}</h5>
       {posts.map(e => (
-        <ListGroup key={e._id} className="my-2">
-          <ListGroup.Item>
-            <h5>{e.title}</h5>
-            <p className="post-text">{e.text}</p>
-          </ListGroup.Item>
-        </ListGroup>
+        <Link to={`posts/${e._id}`} key={e._id} className="my-2 link">
+          <ListGroup>
+            <ListGroup.Item>
+              <h5>{e.title}</h5>
+              <p className="post-text">{e.text}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Link>
       ))}
     </Col>
   </Row>
