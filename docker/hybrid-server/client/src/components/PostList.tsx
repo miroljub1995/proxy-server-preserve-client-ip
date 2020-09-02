aaededd36d3767c8ecf4cca8f398456ac1112442
Miroljub <miroljub1995@gmail.com>
2020-09-02 18:15:19 +0200

    some ui changes

--- docker/hybrid-server/client/src/components/PostList.tsx
+++ docker/hybrid-server/client/src/components/PostList.tsx
@@ -9,14 +9,14 @@ export default ({ posts, title }: { posts: Post[], title: string }) => (
     <Col className="justify-content-center">
       <h5 className="mb-4 text-center">{title}</h5>
       {posts.map(e => (
-        <Link to={`posts/${e._id}`} key={e._id} className="my-2 link">
-          <ListGroup>
+        <ListGroup className="my-3">
+          <Link to={`posts/${e._id}`} key={e._id} className="link">
             <ListGroup.Item>
               <h5>{e.title}</h5>
               <p className="post-text">{e.text}</p>
             </ListGroup.Item>
-          </ListGroup>
-        </Link>
+          </Link>
+        </ListGroup>
       ))}
     </Col>
   </Row>
