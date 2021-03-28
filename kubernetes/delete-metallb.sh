d70a142f85270061fbb62354a75b8dcf4ac68552
miroljub1995 <46632388+miroljub1995@users.noreply.github.com>
2021-03-28 19:22:54 +0200

    Create delete-metallb.sh

---
+++ kubernetes/delete-metallb.sh
@@ -0,0 +1,2 @@
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
