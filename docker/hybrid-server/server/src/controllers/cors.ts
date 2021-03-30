7d31f7edeb8394b0a34cc0fac1993c870feb2ad9
Miroljub <miroljub1995@gmail.com>
2021-03-30 10:24:05 +0200

    changed cross origin

--- docker/hybrid-server/server/src/controllers/cors.ts
+++ docker/hybrid-server/server/src/controllers/cors.ts
@@ -4,8 +4,18 @@ import type { CORSConfig } from "abc/middleware/cors.ts";
 
 import type { Application } from "abc/mod.ts";
 
+const allowedOrigins = [
+  "192.168.0.200:8083",
+  "192.168.0.200:8084",
+  "192.168.0.200:8184",
+  "dunja-home.ddns.net:8083",
+  "dunja-home.ddns.net:8084",
+  "dunja-home.ddns.net:8184",
+  "localhost:3000"
+]
+
 const config: CORSConfig = {
-  allowOrigins: ["*"],
+  allowOrigins: allowedOrigins,
   allowHeaders: [Header.ContentType, Header.Cookie, Header.SetCookie, Header.Accept, Header.Origin],
   allowMethods: [HttpMethod.Get, HttpMethod.Post, HttpMethod.Delete, HttpMethod.Put],
   allowCredentials: true
