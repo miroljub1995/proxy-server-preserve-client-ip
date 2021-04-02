885353343af83c2d1cdecc14fb4e3c6c055a0664
Miroljub <miroljub1995@gmail.com>
2021-04-03 00:02:28 +0200

    single db instance

--- docker/hybrid-server/server-node/src/database/database.ts
+++ docker/hybrid-server/server-node/src/database/database.ts
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const dbURI = process.env['DB_URI'] || "mongodb://localhost:27017"
 
+const db = mongoose.createConnection(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(con => con.useDb('blog'))
+
 export default async function dbClient() {
-  const con = await mongoose.createConnection(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
-  return con.useDb('blog')
+  return await db
 }
