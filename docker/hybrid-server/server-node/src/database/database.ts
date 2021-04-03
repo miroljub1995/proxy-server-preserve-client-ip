38ef54cf88a05c4ad655f1bc63bd95daa5c931c9
Miroljub <miroljub1995@gmail.com>
2021-04-03 10:31:13 +0200

    user blog in connection

--- docker/hybrid-server/server-node/src/database/database.ts
+++ docker/hybrid-server/server-node/src/database/database.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const dbURI = process.env['DB_URI'] || "mongodb://localhost:27017"
 
-export const con = mongoose.createConnection(dbURI, {
+export const con = mongoose.createConnection(dbURI + "/blog", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
-}).then(c => c.useDb('blog'))
+})
