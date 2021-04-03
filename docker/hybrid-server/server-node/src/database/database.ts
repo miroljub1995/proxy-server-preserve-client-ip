bb57395b4b7a0748559fc0ddc34d4d5d8c1efd89
Miroljub <miroljub1995@gmail.com>
2021-04-03 11:01:48 +0200

    fix db

--- docker/hybrid-server/server-node/src/database/database.ts
+++ docker/hybrid-server/server-node/src/database/database.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const dbURI = process.env['DB_URI'] || "mongodb://localhost:27017"
 
-export const con = mongoose.createConnection(dbURI + "/blog", {
+export const con = mongoose.createConnection(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true
+  useCreateIndex: true,
+  dbName: 'blog'
 })
